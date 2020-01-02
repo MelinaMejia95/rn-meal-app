26f39f6e056d2e65d7ae393af91edc9d3cda8b72
melinamejia95 <melinamejia95@gmail.com>
2020-01-02 09:52:24 -0500

    Adding a stack navigator

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const CategoriesScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>Categories Screen</Text>
+      <Button title='Go to Meals!' onPress={() => {
+        props.navigation.navigate('CategoryMeals');
+      }} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoriesScreen;
